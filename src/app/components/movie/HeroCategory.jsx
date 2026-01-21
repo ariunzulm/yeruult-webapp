569e1f9bb055bbea1348acc31db6ebf88dcd99e7
ariunzulm <munkhbatt.ariunzul@gmail.com>
2026-01-21 17:50:56 +0800

    commit

--- src/app/components/movie/HeroCategory.jsx
+++ src/app/components/movie/HeroCategory.jsx
@@ -1,3 +1,6 @@
+import PlayerCardList from "./PlayerCard";
+
+
 export default function HeroCategory() {
   return (
     <section className="w-full min-h-screen bg-gray-950">
@@ -5,6 +8,9 @@ export default function HeroCategory() {
         <button>Active</button>
         <button>Scheduled</button>
         <button>Completed</button>
+      </div>
+      <div className="w-full px-8 ls:px-16 pb-8 md:pb-12">
+        <PlayerCardList />
       </div>
     </section>
   );
